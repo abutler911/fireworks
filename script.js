2bdfc94076befee18658b7e38f5918d79fd1c08d
Andrew Butler <abutler911@gmail.com>
2023-11-27 11:37:56 -0700

    organize

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const container = document.getElementById("fireworksContainer");
 const canvas = document.createElement("canvas");
 container.appendChild(canvas);
-
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -12,15 +11,12 @@ class Firework {
     this.y = y;
     this.targetY = targetY;
     this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    this.velocity = -Math.random() * 3 - 4; // Negative for upward movement
+    this.velocity = -Math.random() * 3 - 4;
   }
 
   update() {
     this.y += this.velocity;
-
-    // Check if firework has reached its peak (targetY)
     if (this.y <= this.targetY) {
-      // TODO: Handle the explosion
       this.explode();
     }
   }
@@ -34,7 +30,6 @@ class Firework {
 
   explode() {
     for (let i = 0; i < 30; i++) {
-      // Create 30 particles
       particles.push(new Particle(this.x, this.y, this.color));
     }
   }
@@ -47,16 +42,16 @@ class Particle {
     this.color = color;
     this.radius = Math.random() * 2 + 1;
     this.velocity = {
-      x: Math.random() - 0.5, // Random direction
+      x: Math.random() - 0.5,
       y: Math.random() - 0.5,
     };
-    this.alpha = 1; // For fade-out effect
+    this.alpha = 1;
   }
 
   update() {
     this.x += this.velocity.x;
     this.y += this.velocity.y;
-    this.alpha -= 0.01; // Gradually fade out
+    this.alpha -= 0.01;
   }
 
   draw() {
@@ -75,12 +70,10 @@ let particles = [];
 
 function animate() {
   requestAnimationFrame(animate);
-  ctx.fillStyle = "rgba(0, 0, 0, 0.1)"; // Trail effect
+  ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  // Handle fireworks
   if (Math.random() < 0.05) {
-    // Random chance to spawn a firework
     const x = Math.random() * canvas.width;
     const targetY = (Math.random() * canvas.height) / 2;
     fireworks.push(new Firework(x, canvas.height, targetY));
@@ -90,20 +83,17 @@ function animate() {
     const firework = fireworks[i];
     firework.update();
     firework.draw();
-
     if (firework.y <= firework.targetY) {
-      fireworks.splice(i, 1); // Remove firework after explosion
+      fireworks.splice(i, 1);
     }
   }
 
-  // Handle particles
   for (let i = particles.length - 1; i >= 0; i--) {
     const particle = particles[i];
     particle.update();
     particle.draw();
-
     if (particle.alpha <= 0) {
-      particles.splice(i, 1); // Remove particle after it fades out
+      particles.splice(i, 1);
     }
   }
 }
