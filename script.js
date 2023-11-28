08098d6bd79e00a4d0e02e96ffe4d64adef758eb
Andrew Butler <abutler911@gmail.com>
2023-11-27 21:19:42 -0700

    refactored

--- script.js
+++ script.js
@@ -1,67 +1,53 @@
-const container = document.getElementById("fireworksContainer");
-const canvas = document.createElement("canvas");
-container.appendChild(canvas);
-const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
 class Firework {
-  constructor(x, y, targetY) {
+  constructor(x, y, targetY, ctx) {
     this.x = x;
     this.y = y;
     this.targetY = targetY;
+    this.ctx = ctx;
     this.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    this.velocity = {
-      x: (Math.random() - 0.5) * 1, // Random horizontal velocity
-      y: -Math.random() * 2 - 2, // Vertical velocity
-    };
-    this.gravity = 0.02; // Gravity factor
+    this.velocity = { x: (Math.random() - 0.5) * 1, y: -Math.random() * 2 - 2 };
+    this.gravity = 0.02;
     this.exploded = false;
   }
 
   update() {
-    // Reduce the vertical velocity each frame to simulate gravity
     if (this.velocity.y < 0) {
-      // Only reduce if the firework is moving upwards
       this.velocity.y += this.gravity;
     }
-
-    // Update position
     this.x += this.velocity.x;
     this.y += this.velocity.y;
-
-    // Check if the firework has reached its peak and should explode
     if ((this.velocity.y >= 0 || this.y <= this.targetY) && !this.exploded) {
       this.explode();
     }
   }
 
   draw() {
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
-    ctx.fill();
+    this.ctx.fillStyle = this.color;
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
+    this.ctx.fill();
   }
 
   explode() {
-    this.exploded = true; // Set the flag when exploded
+    this.exploded = true;
     for (let i = 0; i < 30; i++) {
-      particles.push(new Particle(this.x, this.y, this.color));
+      particles.push(new Particle(this.x, this.y, this.color, this.ctx));
     }
   }
 }
 
 class Particle {
-  constructor(x, y, color) {
+  constructor(x, y, color, ctx) {
     this.x = x;
     this.y = y;
     this.color = color;
+    this.ctx = ctx;
     this.radius = Math.random() * 2 + 1;
     this.velocity = {
-      x: (Math.random() - 0.5) * 6, // Slower horizontal spread
-      y: (Math.random() - 0.5) * 6, // Slower vertical initial velocity
+      x: (Math.random() - 0.5) * 6,
+      y: (Math.random() - 0.5) * 6,
     };
-    this.gravity = 0.1; // Gravity pulling particles down
+    this.gravity = 0.1;
     this.alpha = 1;
   }
 
@@ -73,24 +59,26 @@ class Particle {
   }
 
   draw() {
-    ctx.save();
-    ctx.globalAlpha = this.alpha;
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.restore();
+    this.ctx.save();
+    this.ctx.globalAlpha = this.alpha;
+    this.ctx.fillStyle = this.color;
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    this.ctx.fill();
+    this.ctx.restore();
   }
 }
 
+const container = document.getElementById("fireworksContainer");
+const canvas = document.createElement("canvas");
+container.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 let fireworks = [];
 let particles = [];
-let maxFireworks = 4;
-let fps = 30;
-let now;
-let then = Date.now();
-let interval = 1000 / fps;
-let delta;
+const maxFireworks = 4;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -98,50 +86,42 @@ function animate() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   if (Math.random() < 0.02 && fireworks.length < maxFireworks) {
-    let newFireworksCount = Math.floor(
+    const newFireworksCount = Math.floor(
       Math.random() * (maxFireworks - fireworks.length + 1)
     );
     for (let j = 0; j < newFireworksCount; j++) {
       const x = Math.random() * canvas.width;
-
-      // Calculate targetY to be between 2/3 to 3/4 of the screen height
-      const targetYRangeStart = (canvas.height * 1) / 4;
-      const targetYRangeEnd = (canvas.height * 1) / 3;
       const targetY =
-        targetYRangeStart +
-        Math.random() * (targetYRangeEnd - targetYRangeStart);
-
-      fireworks.push(new Firework(x, canvas.height, targetY));
+        canvas.height / 4 +
+        Math.random() * (canvas.height / 3 - canvas.height / 4);
+      fireworks.push(new Firework(x, canvas.height, targetY, ctx));
     }
   }
 
-  for (let i = fireworks.length - 1; i >= 0; i--) {
-    const firework = fireworks[i];
+  fireworks.forEach((firework, index) => {
     firework.update();
     firework.draw();
     if (firework.exploded) {
-      fireworks.splice(i, 1);
+      fireworks.splice(index, 1);
     }
-  }
+  });
 
-  for (let i = particles.length - 1; i >= 0; i--) {
-    const particle = particles[i];
+  particles.forEach((particle, index) => {
     particle.update();
     particle.draw();
     if (particle.alpha <= 0) {
-      particles.splice(i, 1);
+      particles.splice(index, 1);
     }
-  }
+  });
 }
 
 function drawStarryBackground() {
-  const starCount = (canvas.width + canvas.height) / 2; // Adjust the number of stars based on canvas size
-
+  const starCount = (canvas.width + canvas.height) / 2;
   ctx.fillStyle = "white";
   for (let i = 0; i < starCount; i++) {
     const x = Math.random() * canvas.width;
     const y = Math.random() * canvas.height;
-    const radius = Math.random() * 1.5; // Random radius for stars
+    const radius = Math.random() * 1.5;
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.fill();
